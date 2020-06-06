cbd84f0797aebf22a86c878f54ab2fbdec14a909
novakvova <novakvova@gmail.com>
2020-06-06 14:23:17 +0300

    Update text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello my freands
+                    Hello my friends
                 </div>
 
                 <div class="links">
