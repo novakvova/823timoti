6fc17f61c8230f022cb68a11fc2bb3be91b65f59
novakvova <novakvova@gmail.com>
2020-06-06 14:20:53 +0300

    Add text home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello my freands
                 </div>
 
                 <div class="links">
